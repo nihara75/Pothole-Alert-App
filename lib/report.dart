710f1ab295d9915b621112602ae810c972935909
nihara75 <niharar25@gmail.com>
2020-12-07 23:10:44 +0530

    Added small changes to reporting

--- lib/report.dart
+++ lib/report.dart
@@ -1,38 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mailer/flutter_mailer.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'constant.dart';
+import 'home.dart';
+class Report extends StatefulWidget {
 
-class Report extends StatelessWidget {
+
+  @override
+  _ReportState createState() => _ReportState();
+}
+
+class _ReportState extends State<Report> {
   String platformResponse;
+  final _firestore=FirebaseFirestore.instance;
+
+  static String locat=" ";
   final MailOptions mailOptions = MailOptions(
-    body: 'a long body for the email <br> with a subset of HTML',
-    subject: 'the Email Subject',
-    recipients: ['example@example.com'],
+    body: locat,
+    subject: 'Potholes Nearby ',
+    recipients: ['niharar25@gmail.com'],
     isHTML: true,
-    bccRecipients: ['other@example.com'],
+    /* bccRecipients: ['other@example.com'],
     ccRecipients: ['third@example.com'],
-    attachments: [ 'path/to/image.png', ],
+    attachments: [ 'path/to/image.png', ],*/
   );
-void sendmail()async{
-  final MailerResponse response = await FlutterMailer.send(mailOptions);
-  switch (response) {
-  case MailerResponse.saved: /// ios only
-  platformResponse = 'mail was saved to draft';
-  break;
-  case MailerResponse.sent: /// ios only
-  platformResponse = 'mail was sent';
-  break;
-  case MailerResponse.cancelled: /// ios only
-  platformResponse = 'mail was cancelled';
-  break;
-  case MailerResponse.android:
-  platformResponse = 'intent was successful';
-  break;
-  default:
-  platformResponse = 'unknown';
-  break;
-  }}
+
+
+  void getd() async{
+   await _firestore.collection("location").get().then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        //print(result.data());
+        setState(() {
+          locat=result.data().toString();
+        });
+
+        print(locat);
+      });
+    });
+   sendmail();
+   /*Future.delayed(const Duration(seconds: 5), () {
+
+// Here you can write your code
+
+
+
+   });*/
+
+
+  }
+
+  void sendmail()async{
+
+    /*print(locat);
+    print(locat);
+    print(locat);*/
+    final MailerResponse response = await FlutterMailer.send(mailOptions);
+    switch (response) {
+      case MailerResponse.saved: /// ios only
+      ///
+        setState(() {
+          platformResponse = 'mail was saved to draft';
+        });
+
+        break;
+      case MailerResponse.sent: /// ios only
+        setState(() {
+          platformResponse = 'mail was sent';
+        });
+        break;
+      case MailerResponse.cancelled: /// ios only
+        setState(() {
+          platformResponse = 'mail was cancelled';
+        });
+        break;
+      case MailerResponse.android:
+        setState(() {
+          platformResponse = 'intent was successful';
+        });
+        break;
+      default:
+        setState((){platformResponse = 'unknown';});
+
+        break;
+    }
+
+
+    Alert(
+      context: context,
+      type: AlertType.info,
+      title: "STATUS",
+      desc: platformResponse,
+      buttons: [
+        DialogButton(
+          child: Text(
+            "DONE",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          width: 120,
+          color:kPrimaryColor
+        )
+      ],
+    ).show();
+  }
+
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getd();
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(child: Scaffold());
   }
 }
+
+
